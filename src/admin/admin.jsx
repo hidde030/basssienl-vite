46c4e11c867a93b3237001b96267c60228b96119
hidde030 <glansdorphidde@gmail.com>
2023-01-20 17:01:38 +0100

    update admin form

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -1,98 +1,132 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Admin({ selectedUser, setShowModal }) {
-
-  const [formData, setFormData] = useState({...selectedUser});
+  const [formData, setFormData] = useState({ ...selectedUser });
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
-  }
+  };
   const handleSave = async (event) => {
     try {
-      const response = await fetch('http://localhost:3000/api/update', {
-        method: 'PUT',
+      const response = await fetch("http://localhost:3000/api/update", {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        
-        body: JSON.stringify(formData)
+
+        body: JSON.stringify(formData),
       });
       const data = await response.json();
       console.log(data);
       if (response.ok) {
         // handle success
-        console.log("Data saved successfully")
+        console.log("Data saved successfully");
       } else {
         // handle error
-        console.log("Error in saving data")
+        console.log("Error in saving data");
       }
     } catch (error) {
       // handle error
-      console.log(error)
+      console.log(error);
     }
-    setShowModal(false)
+    setShowModal(false);
+  };
 
-  }
-  
   return (
+    <div className="fixed inset-0 top-12 grid text-center text-whte">
+      <form className="bg-white flex flex-col" onSubmit={handleSave}>
+        <div class="relative mb-2">
+          <label for="name">Name:</label>
+          <input
+          disabled
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div class="relative mb-2">
+          <label for="nationality">nationality:</label>
 
-    <div className='fixed inset-0 top-12 grid text-center text-whte'>
-    <form className='bg-white' onSubmit={handleSave} >
-      <label>
-        Name:
-        <input type="text" name="name" value={formData.name} onChange={handleChange} />
-      </label>
-      <br />
-    
-      <label>
-      nationality:
-        <input type="text" name="nationality" value={formData.nationality} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-      Rating:
-        <input type="text" name="rating" value={formData.rating} onChange={handleChange} />
-      </label>
-      <br />
-    
-      <label>
-      rank:
-        <input type="text" name="rank" value={formData.rank} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-      faceit:
-        <input type="text" name="faceit" value={formData.faceit} onChange={handleChange} />
-      </label>
-      <br />
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="nationality"
+            value={formData.nationality}
+            onChange={handleChange}
+          />
+        </div>
+        <div class="relative mb-2">
+          <label for="rating">Rating:</label>
 
-      <label>
-      quality:
-        <input type="text" name="quality" value={formData.quality} onChange={handleChange} />
-      </label>
-      <br />
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="rating"
+            value={formData.rating}
+            onChange={handleChange}
+          />
+        </div>
+        <div class="relative mb-2">
+          <label for="rank">rank:</label>
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="rank"
+            value={formData.rank}
+            onChange={handleChange}
+          />
+        </div>
+        <div class="relative mb-2">
+          <label for="faceit">faceit:</label>
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="faceit"
+            value={formData.faceit}
+            onChange={handleChange}
+          />
+        </div>
+        <div class="relative mb-2">
+          <label for="quality">quality:</label>
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="quality"
+            value={formData.quality}
+            onChange={handleChange}
+          />
+        </div>
 
-      <label>
-      quality:
-        <input type="text" name="weakness" value={formData.weakness} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-      img:
-        <input type="text" name="img" value={formData.img} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-      steam_url:
-        <input type="text" name="steam_url" value={formData.steam_url} onChange={handleChange} />
-      </label>
-      <br />
-      <button onClick={handleSave}>Save</button>
-      <button onClick={() => setShowModal(false)}>Close</button>
-      
+        <div class="relative mb-2">
+          <label for="img">img:</label>
 
-    </form>
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="img"
+            value={formData.img}
+            onChange={handleChange}
+          />
+        </div>
+        <div class="relative mb-2">
+          <label for="steam_url">steam_url:</label>
+
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="steam_url"
+            value={formData.steam_url}
+            onChange={handleChange}
+          />
+        </div>
+        <button                                    class="bg-admin hover:bg-green-700 text-white font-bold py-2 px-6 rounded my-6 max-w-sm mx-auto"
+ onClick={handleSave}>Save</button>
+        <button                                      class="bg-admin hover:bg-green-700 text-white font-bold py-2 px-6 rounded my-6 max-w-sm mx-auto"
+onClick={() => setShowModal(false)}>Close</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Admin;
