26b9d7659c3eddd52f8f6cbd58111db6ff734538
Hidde Glansdorp <hidde.glansdorp@student.hu.nl>
2023-01-20 20:57:29 +0100

    weakness added

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -98,6 +98,16 @@ function Admin({ selectedUser, setShowModal }) {
             onChange={handleChange}
           />
         </div>
+        <div class="relative mb-2">
+          <label className="text-white"  for="weakness">weakness:</label>
+          <input
+            class="block appearance-none w-full bg-primary-grey border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            name="weakness"
+            value={formData.weakness}
+            onChange={handleChange}
+          />
+        </div>
 
         <div class="relative mb-2">
           <label className="text-white"  for="img">img:</label>
