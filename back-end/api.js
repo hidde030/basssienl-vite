22cc9bee24559d9faed9c93ad0c491f438787d64
hidde030 <glansdorphidde@gmail.com>
2024-06-02 12:40:07 +0200

    chore: Update API to remove serving of static files and fix login functionality

--- back-end/api.js
+++ back-end/api.js
@@ -38,13 +38,7 @@ const auth = (req, res, next) => {
     next()
   }
 }
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./dist/index.html"), function (err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+
 app.post("/api/auth", auth, (req, res) => {
   res.status(200).send("AUTHENTICATED")
   // redirect to the main page
